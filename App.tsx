24cb3e2b29fd32ca5274fd1e2aa9e7b173cbe134
chente09 <chentenengerc@gmail.com>
2024-07-05 01:07:20 -0500

    apifailed

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 import Navegacion from './navigation/MainNavigator';
+import ApiFailScreen from './screens/ApiFailScreen';
 
 export default function App() {
   return (
     <Navegacion/>
+    // <ApiFailScreen/>
   );
 }
 
